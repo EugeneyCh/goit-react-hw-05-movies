f128cd8865b8bdececa8fb0eaf7e40f6fa54534e
EugeneyCh <eugeney@ua.fm>
2023-07-16 18:48:48 +0300

    First_trying

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,24 @@
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 // import { lazy } from 'react';
+import Home from 'pages/Home';
+import Layout from './layout/Layout';
+import Movies from 'pages/Movies';
+import MovieDetails from 'pages/MovieDetails';
+import Cast from './cast/Cast';
+import Reviews from './reviews/Reviews';
 
 const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      goit-react-hw-05-movies
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
 export default App;
