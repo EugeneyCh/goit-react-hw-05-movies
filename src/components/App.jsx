8c6a10ff5b6c8ae92c25d9f314f6e306f3e37beb
EugeneyCh <eugeney@ua.fm>
2023-07-16 20:58:24 +0300

    Third_Trying

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 // import { lazy } from 'react';
-// import Layout from './layout/Layout';
+import Layout from './layout/Layout';
 import Home from 'pages/Home';
 import Movies from 'pages/Movies';
 import MovieDetails from 'pages/MovieDetails';
@@ -11,15 +11,15 @@ function App() {
   return (
     <div>
       <Routes>
-        {/* <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} /> */}
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
-        {/* </Route> */}
       </Routes>
     </div>
   );
